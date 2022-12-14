90014de8d6afc66fa01393315a3449ecce050a71
unknown <anaclaucf@hotmail.com>
2022-12-14 13:25:37 -0300

    feature: also upsize when image is smaller than expected

--- main.py
+++ main.py
@@ -11,11 +11,33 @@ def make_path(path):
     return '/'.join(path)
 
 
+def upsize(image):
+    width = image.size[0]
+    height = image.size[1]
+    ratio = width/height
+    if width > height:
+        new_size = (TELEGRAM_MAX_SIZE, TELEGRAM_MAX_SIZE/ratio)
+    else:
+        new_size = (TELEGRAM_MAX_SIZE*ratio, TELEGRAM_MAX_SIZE)
+    image.resize(new_size)
+
+
+def downsize(image):
+    image.thumbnail((TELEGRAM_MAX_SIZE, TELEGRAM_MAX_SIZE))
+
+
 def resize(name):
     image = Image.open(make_path([INPUT_PATH, name]))
-    image.thumbnail((TELEGRAM_MAX_SIZE, TELEGRAM_MAX_SIZE))
+    print(f'----------------')
+    print(f'Starting image {name}')
+    if image.size[0] >= TELEGRAM_MAX_SIZE or image.size[1] >= TELEGRAM_MAX_SIZE:
+        print(f'Downsizing image')
+        downsize(image)
+    else:
+        print(f'Upsizing image')
+        upsize(image)
     image.save(make_path([OUTPUT_PATH, name]), quality=20, optimize=True)
-    print(f'Saved {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Saved')
 
 
 def fetch_files():
